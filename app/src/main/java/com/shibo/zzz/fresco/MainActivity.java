aa2cddc19887387ac24eabec3a22d2f4bbcaf80b
shibo <1079771359@qq.com>
2016-09-20 08:57:45 +0800

    修改了MainActivity

--- app/src/main/java/com/shibo/zzz/fresco/MainActivity.java
+++ app/src/main/java/com/shibo/zzz/fresco/MainActivity.java
@@ -1,12 +1,21 @@
 package com.shibo.zzz.fresco;
 
+import android.graphics.drawable.Animatable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.facebook.common.logging.FLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.image.QualityInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.lidroid.xutils.ViewUtils;
@@ -33,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.parse(img_url0);
 
         draweeView.setImageURI(uri);
-        requestImage();
+//        requestImage();
+        moreImages();
 
 
     }
@@ -56,5 +66,55 @@ public class MainActivity extends AppCompatActivity {
 
 
 draweeView.setController(controller);
+    }
+
+
+    private void moreImages(){
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(
+                    String id,
+                    @Nullable ImageInfo imageInfo,
+                    @Nullable Animatable anim) {
+                if (imageInfo == null) {
+                    return;
+                }
+                QualityInfo qualityInfo = imageInfo.getQualityInfo();
+                FLog.d("Final image received! " +
+                                "Size %d x %d",
+                        "Quality level %d, good enough: %s, full quality: %s",
+                        imageInfo.getWidth(),
+                        imageInfo.getHeight(),
+                        qualityInfo.getQuality(),
+                        qualityInfo.isOfGoodEnoughQuality(),
+                        qualityInfo.isOfFullQuality());
+                Log.e("------------->","Final image received! " +
+                                "Size :"+ imageInfo.getWidth()+
+                        ",Quality :"+  qualityInfo.getQuality()+
+                       " ,good enough: "+ qualityInfo.isOfGoodEnoughQuality()
+                        +", full quality: "+  qualityInfo.isOfFullQuality());
+            }
+
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                FLog.d("","Intermediate image received");
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                FLog.e(getClass(), throwable, "Error loading %s", id);
+            }
+        };
+
+
+
+
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setControllerListener(controllerListener)
+                .setLowResImageRequest(ImageRequest.fromUri(img_url6))
+                .setImageRequest(ImageRequest.fromUri(img_url5))
+                .setOldController(draweeView.getController())
+                .build();
+        draweeView.setController(controller);
     }
 }
